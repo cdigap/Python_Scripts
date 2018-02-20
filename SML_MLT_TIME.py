39bef00d0e0a180e486c569085dba6f9b007bd28
cdigap <36169806+cdigap@users.noreply.github.com>
2018-02-20 21:22:58 +0000

    Create SML_MLT_TIME.py

---
+++ SML_MLT_TIME.py
@@ -0,0 +1,40 @@
+# Ashok Gangadharan 2018-02-20
+
+#https://projecteuler.net/problem=5 With time taken
+
+# Importing Time Library
+import time
+
+# Number initializatoin
+n = 0
+i = 1
+
+# Getting the Start time
+st = time.time()
+
+#While Loop
+while n==0:
+
+    # Incrementing i 
+    i=i+1
+    
+    # Checking if the number is divisible by a 1 to 20
+    if  i%1==0 and i%2==0 and i%3==0 and i%4==0 and \
+        i%5==0 and i%6==0 and i%7==0 and i%8==0 and \
+        i%9==0 and i%10==0 \
+        and \
+        i%11==0 and i%12==0 and i%13==0 and i%14==0 and \
+        i%15==0 and i%16==0 and i%17==0 and i%18==0 and \
+        i%19==0 and i%20==0:
+        # Getting the End time
+        ed = time.time()
+        
+       # Once we get the result making sure we are off the while loop
+        n=1
+        
+# calculating the time taken for the script to run.
+t=ed-st
+
+print("Smallest positive number that is evenly divisible by all of the numbers from 1 to 20: ", i," time : ", t)
+
+#Smallest positive number that is evenly divisible by all of the numbers from 1 to 20:  232792560  time :  75.28518438339233
